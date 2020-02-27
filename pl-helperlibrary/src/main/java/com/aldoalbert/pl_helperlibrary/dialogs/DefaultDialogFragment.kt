96e73d2d552b18986d12b2f3354ac4794e1b413f
aldo@csgroup.mx <AAlbert1992!>
2020-02-27 13:35:12 -0600

    #fix Correcciones de dialogs

--- pl-helperlibrary/src/main/java/com/aldoalbert/pl_helperlibrary/dialogs/DefaultDialogFragment.kt
+++ pl-helperlibrary/src/main/java/com/aldoalbert/pl_helperlibrary/dialogs/DefaultDialogFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.DialogFragment
@@ -15,56 +16,51 @@ import com.aldoalbert.pl_helperlibrary.R
  */
 class DefaultDialogFragment : DialogFragment() {
 
-
-
-    private var title: String? = null
-    private var message: String? = null
-    private var type : Int? = null
+    private val title: String by lazy { arguments!!.getString(ARG_TITLE, "") }
+    private val message: String by lazy { arguments!!.getString(ARG_MESSAGE, "") }
+    private val type : Int by lazy { arguments!!.getInt(ARG_TYPE) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        title = arguments?.getString("title")
-        message = arguments?.getString("message")
-
         val style = STYLE_NO_FRAME
         val theme = R.style.DialogTheme
         setStyle(style, theme)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.layout_dialog, container, false)
+        val view = inflater.inflate(type, container, false)
 
-        val _tv_title = view.findViewById<View>(R.id.tv_title) as TextView
+        val _tv_title = view.findViewById<TextView>(R.id.tv_title)
         _tv_title.text = title
 
-        val _tv_message = view.findViewById<View>(R.id.tv_message) as TextView
+        val _tv_message = view.findViewById<TextView>(R.id.tv_message)
         _tv_message.text = message
 
         when (type) {
             SUCCESS_TYPE -> {
-                val btnAccept = view.findViewById<View>(R.id.buttonAccept)
+                val btnAccept = view.findViewById<Button>(R.id.buttonAccept)
                 btnAccept.setOnClickListener {
                     dismiss()
                 }
             }
             ERROR_TYPE -> {
-                val btnAccept = view.findViewById<View>(R.id.buttonAccept)
+                val btnAccept = view.findViewById<Button>(R.id.buttonAccept)
                 btnAccept.setOnClickListener {
                     dismiss()
                 }
             }
             ERROR_NETWORK_TYPE -> {
-                val btnAccept = view.findViewById<View>(R.id.buttonAccept)
+                val btnAccept = view.findViewById<Button>(R.id.buttonAccept)
                 btnAccept.setOnClickListener {
                     dismiss()
                 }
             }
             WARNING_TYPE -> {
-                val btnAccept = view.findViewById<View>(R.id.buttonAccept)
+                val btnAccept = view.findViewById<Button>(R.id.buttonAccept)
                 btnAccept.setOnClickListener {
                     dismiss()
                 }
-                val btnCancel = view.findViewById<View>(R.id.buttonCancel)
+                val btnCancel = view.findViewById<Button>(R.id.buttonCancel)
                 btnCancel.setOnClickListener {
                     dismiss()
                 }
@@ -80,11 +76,15 @@ class DefaultDialogFragment : DialogFragment() {
 
     companion object {
 
-        public val SUCCESS_TYPE = 1
-        public val ERROR_TYPE = 2
-        public val ERROR_NETWORK_TYPE = 3
-        public val WARNING_TYPE = 4
-        public val LOADING_TYPE = 5
+        val SUCCESS_TYPE = R.layout.layout_success_dialog
+        val ERROR_TYPE = R.layout.layout_error_dialog
+        val ERROR_NETWORK_TYPE = R.layout.layout_error_network_dialog
+        val WARNING_TYPE = R.layout.layout_warning_dialog
+        val LOADING_TYPE = R.layout.layout_loading_dialog
+
+        const val ARG_TITLE = "title"
+        const val ARG_MESSAGE= "message"
+        const val ARG_TYPE = "type"
 
         /**
          * Create a new instance of CustomDialogFragment.
@@ -94,9 +94,9 @@ class DefaultDialogFragment : DialogFragment() {
 
             // Supply num input as an argument.
             val args = Bundle()
-            args.putString("title", title)
-            args.putString("message", message)
-            args.putInt("type", type)
+            args.putString(ARG_TITLE, title)
+            args.putString(ARG_MESSAGE, message)
+            args.putInt(ARG_TYPE, type)
             f.arguments = args
 
             return f
